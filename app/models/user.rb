66b443eb640ab2cfc36b76ad045514c93a06452b
andre-jun <andrejhirata@usp.br>
2025-11-14 17:59:44 -0300

    WIP: Student report Submission

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-      
-  # Stackoverflow falou pra ter isso mas vai ficar comentado enquanto é só magia negra
-  #attr_accessible :password, :password_confirmation
 
-  has_one :Administrator
-  has_one :Student
+  # Stackoverflow falou pra ter isso mas vai ficar comentado enquanto é só magia negra
+  # attr_accessible :password, :password_confirmation
+
+  has_one :administrator
+  has_one :student
   has_one :professor
 end
